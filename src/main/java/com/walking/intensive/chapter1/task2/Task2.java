bd6dc64a5cd09d9cd925c853d65c9548c2a7d30c
artem <123@123.rete>
2024-11-18 18:21:36 +0300

    Решил вторую задачу.

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -32,13 +32,62 @@ package com.walking.intensive.chapter1.task2;
  * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
  */
 public class Task2 {
+    private static final int COUNT_FLATS_ON_FLOOR = 4;
+
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println(getFlatLocation(10, 10, 23));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        if (floorAmount < 0 || entranceAmount < 0 || flatNumber < 0) {
+            return "Некорректные входные данные";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        if (flatNumber > (COUNT_FLATS_ON_FLOOR * floorAmount * entranceAmount)) {
+            return "Такой квартиры не существует";
+        }
+
+        // Узнаем подъезд
+
+        int floorCounts = COUNT_FLATS_ON_FLOOR * floorAmount;
+        int nEntrance = flatNumber / floorCounts;
+
+        if (flatNumber % floorAmount != 0) {
+            nEntrance++;
+        }
+
+        // Узнаем этаж
+
+        int nFloor = (flatNumber - ((nEntrance - 1) * floorCounts)) / 4;
+
+        // Узнаем направление относительно лифта
+
+        String elevatorPosition;
+
+        int floorIndex = flatNumber % COUNT_FLATS_ON_FLOOR;
+
+        switch (floorIndex) {
+            case 1, 2:
+                elevatorPosition = "слева";
+                break;
+            default:
+                elevatorPosition = "справа";
+        }
+
+        // Узнаем сторону квартиры
+
+        String flatPosition;
+
+        if (elevatorPosition.equals("слева") && (floorIndex == 1)
+                || (elevatorPosition.equals("справа") && (floorIndex == 3))) {
+            flatPosition = "влево";
+        } else {
+            flatPosition = "вправо";
+        }
+
+        return flatNumber + " кв - " + nEntrance + " подъезд, "
+                + nFloor + " этаж, "
+                + elevatorPosition + " от лифта, "
+                + flatPosition;
     }
 }
