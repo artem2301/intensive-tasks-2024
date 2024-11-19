d529cfbb460574a275d7403b206f1549ca28602c
artem <123@123.rete>
2024-11-19 23:55:33 +0300

    Изменены условия для определения количества решений уравнения. Добавлены 0.0 к переменным для корректной обработки -0.0.

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -25,32 +25,31 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
         double a = 1;
-        double b = 10;
-        double c = 8;
+        double b = 0;
+        double c = 0;
 
         System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
+        if (a == 0 && b == 0 && c == 0) {
+            return "Бесконечное множество решений.";
+        }
+
+        if (a == 0 && b == 0) {
+            return "Количество решений: 0.";
+        }
+
         if (a == 0) {
-            if (b == 0) {
-                if (c == 0) {
-                    return "Бесконечное множество решений.";
-                }
+            double x;
 
-                return "Количество решений: 0.";
+            if (c == 0) {
+                x = c / b;
             } else {
-                double x = 0.0;
-
-                if (c == 0) {
-                    x = c / b;
-                } else {
-                    x = -c / b;
-                }
-
-                return "Количество решений: 1. Корень: " + x;
+                x = -c / b;
             }
+
+            return "Количество решений: 1. Корень: " + (x + 0.0);
         }
 
         double d = b * b - 4 * a * c;
@@ -59,23 +58,23 @@ public class Task4 {
             return "Количество решений: 0.";
         }
 
-        double x1 = 0, x2 = 0;
+        double x1, x2;
 
         if (d == 0) {
             x1 = -b / (2 * a);
 
-            return "Количество решений: 1. Корень: " + x1;
-        } else {
-            x1 = (-b + Math.sqrt(d)) / (2 * a);
-            x2 = (-b - Math.sqrt(d)) / (2 * a);
-
-            if (x2 < x1) {
-                double buf = x1;
-                x1 = x2;
-                x2 = buf;
-            }
-
-            return "Количество решений: 2. Корни: " + x1 + ";" + x2;
+            return "Количество решений: 1. Корень: " + (x1 + 0.0);
         }
+
+        x1 = (-b + Math.sqrt(d)) / (2 * a);
+        x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+        if (x2 < x1) {
+            double buf = x1;
+            x1 = x2;
+            x2 = buf;
+        }
+
+        return "Количество решений: 2. Корни: " + (x1 + 0.0) + ";" + (x2 + 0.0);
     }
 }
