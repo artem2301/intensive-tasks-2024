2a9b10a9bf3a931252af7c9a71f99d2015fd2927
artem <123@123.rete>
2024-11-24 14:06:08 +0300

    Решена задача 11.

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -42,11 +42,46 @@ package com.walking.intensive.chapter3.task11;
  */
 public class Task11 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println(getOddSubArraysElementsSum(new int[] {1, 4, 2, 5, 3}));
     }
 
     static int getOddSubArraysElementsSum(int[] array) {
-        // Ваш код
-        return 0;
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                return -1;
+            }
+        }
+
+        return array.length % 2 == 0 ? getSum(array, array.length - 1, 1) : getSum(array, array.length, 0);
+    }
+
+    private static int getSum(int[] array, int maxLength, int evenLength) {
+        int sum = 0;
+        int currentLength = 1;
+        int currentPos = 0;
+
+        while (currentLength != maxLength + 1) {
+            if (currentLength % 2 == 0) {
+                currentLength++;
+                continue;
+            }
+
+            for (int i = currentPos; i < currentPos + currentLength + evenLength; i++) {
+                sum += array[i];
+            }
+
+            if (currentPos == maxLength - 1 || currentPos + currentLength >= maxLength) {
+                currentPos = 0;
+                currentLength++;
+            } else {
+                currentPos++;
+            }
+        }
+
+        return sum;
     }
 }
