a68a76aae860547f095f9a8b30ceb02a0fbce5a6
artem <123@123.rete>
2024-12-10 19:26:54 +0300

    Решена задача 20.

--- src/main/java/com/walking/intensive/chapter5/task20/Task20.java
+++ src/main/java/com/walking/intensive/chapter5/task20/Task20.java
@@ -24,7 +24,13 @@ package com.walking.intensive.chapter5.task20;
  */
 public class Task20 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println(getDeterminant(new int[][] {
+                {0, 3, 5, 8, 7},
+                {1, 4, 3, 7, 9},
+                {6, 2, 1, 5, 5},
+                {0, 4, 2, 1, 3},
+                {6, 4, 2, 8, 9}
+        }));
     }
 
     /**
@@ -40,8 +46,25 @@ public class Task20 {
      * До тех пор приходится находить обходные пути для обозначения ситуаций, когда что-то пошло не по плану.
      */
     static Integer getDeterminant(int[][] matrix) {
-        // Ваш код
-        return null;
+        if (!isValid(matrix)) {
+            return null;
+        }
+
+        if (matrix.length == 2) {
+            return getDeterminantSize2(matrix);
+        }
+
+        int result = 0;
+
+        for (int j = 0; j < matrix.length; j++) {
+            if (matrix[0][j] == 0) {
+                continue;
+            }
+
+            result += (int) (Math.pow(-1, 2 + j)) * matrix[0][j] * getDeterminant(getNewArray(matrix, j));
+        }
+
+        return result;
     }
 
     /**
@@ -52,7 +75,43 @@ public class Task20 {
      * <p>Логика валидации должна быть определена в этом методе, чтобы не усложнять логику getDeterminant(). При этом
      * getDeterminant() должен использовать isValid().
      */
+    private static int getDeterminantSize2(int[][] matrix) {
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+    }
+
+    private static int[][] getNewArray(int[][] matrix, int y) {
+        int[][] buffer = new int[matrix.length - 1][matrix.length - 1];
+
+        int indexI = 0;
+        int indexJ = 0;
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (j == y) {
+                    continue;
+                }
+
+                buffer[indexI][indexJ++] = matrix[i][j];
+            }
+
+            indexI++;
+            indexJ = 0;
+        }
+
+        return buffer;
+    }
+
     static boolean isValid(int[][] matrix) {
-        return false;
+        if (matrix == null) {
+            return false;
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i].length != matrix.length) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
